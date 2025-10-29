59b3cb7a1b69370ed1ac61b7b11f85836100f389
udhayamalai7-arch <udhayamalai7@gmail.com>
2025-10-29 22:09:17 +0530

    task completed

---
+++ day19/main10.js
@@ -0,0 +1,4 @@
+for(let i=1;i<=10;i++){
+    if(i===10)break;
+    console.log(i)
+}
---
+++ day19/main3.js
@@ -0,0 +1,8 @@
+// even no
+for(let i=2;i<=20;i+=2){
+    console.log(i)
+}
+// odd no
+for(let i=1;i<=19;i+=2){
+    console.log(i)
+}
